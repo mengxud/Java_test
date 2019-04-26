7765362a36e96a7fe935d8bca7251043724bd278
mxd_glb <mengxud_glb@foxmail.com>
2019-04-26 17:18:50 +0800

    huiche2 commit

--- src/hello/HelloGit.java
+++ src/hello/HelloGit.java
@@ -1,9 +1,13 @@
 package hello;
 
 public class HelloGit {
+
     public static void main(String[] args) {
+
         System.out.println("Hello ,I come from IDEA");
         System.out.println("Hello ,I come from IDEA");
         System.out.println("Hello ,I come from IDEA");
+        System.out.println("Hello ,I come from IDEA2");
+        System.out.println("Hello ,I come from IDEA2");
     }
 }
